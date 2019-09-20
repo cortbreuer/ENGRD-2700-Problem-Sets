728a7ae36ca810795b2e91a59f61432be451802c
Cort Breuer <cb732@cornell.edu>
2019-09-20 11:06:19 -0400

    start writing recursion for pump assignment

---
+++ Problem Sets/Problem Set 2/Scripts/recur_pumps.R
@@ -0,0 +1,6 @@
+#recur_pumps returns the number of combinations possible for assigning 
+
+recur_pumps <- function(n, k) {
+  
+  
+}
---
+++ Recitations/Recitation 3/recur_choose.R
@@ -0,0 +1,8 @@
+recur_choose <- function(n, k) {
+  if (n <= 1 | k <= 1) {
+    return(1)
+  } else {
+      return(recur_choose(n-1, k) + 
+  }
+  
+}
