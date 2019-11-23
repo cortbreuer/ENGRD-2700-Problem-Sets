98296b1ea5733ac574e8c862d1f1d4f05cb28440
Cort Breuer <cb732@cornell.edu>
2019-11-23 16:38:32 -0500

    restructure folders

--- Recitations/Recitation 3/recur_choose.R
+++
@@ -1,8 +0,0 @@
-recur_choose <- function(n, k) {
-  if (n <= 1 | k <= 1) {
-    return(1)
-  } else {
-      return(recur_choose(n-1, k) + 
-  }
-  
-}
--- Recitations/Recitation 3/recur_factorial.R
+++
@@ -1,7 +0,0 @@
-recur_factorial <- function(n) {
-  if(n <= 1) {
-    return(1)
-  } else {
-      return(n * recur_factorial(n - 1))
-  }
-}
